0ba2f89d79fd88a126292f803ddd298a54307706
Zhipeng Jia <zjia@cs.utexas.edu>
2020-12-07 21:46:45 -0600

    expected results of hotelresv workload

---
+++ experiments/hotelresv_singlenode/run_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+BASE_DIR=`realpath $(dirname $0)`
+ROOT_DIR=`realpath $BASE_DIR/../..`
+
+HELPER_SCRIPT=$ROOT_DIR/scripts/exp_helper
+
+$HELPER_SCRIPT start-machines
+
+ALL_QPS="3000 4000 5000 6000"
+for qps in $ALL_QPS; do
+    sleep 30
+    $BASE_DIR/run_once.sh qps${qps} $qps
+done
+
+$HELPER_SCRIPT stop-machines
