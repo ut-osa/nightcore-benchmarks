0ec1befec95ca2d9472a51146c4a908a602bcaca
Zhipeng Jia <zhipeng.jia@outlook.com>
2021-02-10 19:58:26 -0600

    Add collect_results script

--- experiments/hipstershop_4node/run_all.sh
+++ experiments/hipstershop_4node/run_all.sh
@@ -4,7 +4,7 @@ ROOT_DIR=`realpath $BASE_DIR/../..`
 
 HELPER_SCRIPT=$ROOT_DIR/scripts/exp_helper
 
-# $HELPER_SCRIPT start-machines
+$HELPER_SCRIPT start-machines
 
 ALL_QPS="4500 5000 5500"
 for qps in $ALL_QPS; do
